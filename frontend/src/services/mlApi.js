5df6ffb75f0459c2e483abde0dfacf6575888b4e
Ravi Kumar <ravikumarasas23@gmail.com>
2026-08-23 23:17:39 +0530

    Use environment variable for ML API URL

--- frontend/src/services/mlApi.js
+++ frontend/src/services/mlApi.js
@@ -1,4 +1,6 @@
-const API_BASE_URL = "http://127.0.0.1:8000";
+import { detectionBaseUrl } from "../Config/config";
+
+const API_BASE_URL = detectionBaseUrl;
 
 async function apiRequest(
   endpoint,
